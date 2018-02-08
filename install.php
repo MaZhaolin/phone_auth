a036cf34a5507a61aa84823b5f608ce4ced2b8fd
insertSweat <769356353@qq.com>
2018-02-08 16:03:07 +0800

    when install, save existing data

--- install.php
+++ install.php
@@ -4,15 +4,77 @@ if(!defined('IN_DISCUZ')) {
 	exit('Access Denied');
 }
 
-$sql = "CREATE TABLE IF NOT EXISTS `" . DB::table('common_vphone') . "` (" .
-  "`phone` bigint(20) unsigned NOT NULL," .
-  "`uid` mediumint(8) unsigned NOT NULL," .
-  "`country_code` char(40) NOT NULL default '86',".
-  "PRIMARY KEY (`phone`)," .
-  "UNIQUE KEY (`uid`)".
-") ENGINE=InnoDB DEFAULT CHARSET=utf8";
+class Install {
+    public static function createTables() {
+        $sql = "CREATE TABLE IF NOT EXISTS `" . DB::table('sessions') . "` (" .
+        "`SessionKey` varchar(32) NOT NULL default '', ".
+        "`SessionArray` blob NOT NULL, ".
+        "`SessionExpTime` int(20) unsigned NOT NULL default '0', ".
+        "PRIMARY KEY (`SessionKey`), ".
+        "KEY `SessionKey` (`SessionKey`) ".
+        ") ENGINE=InnoDB DEFAULT CHARSET=utf8";
 
-runquery($sql);
+        runquery($sql);
 
-$finish = TRUE;
-?>
+        $sql = "CREATE TABLE IF NOT EXISTS `" . DB::table('common_vphone') . "` (" .
+        "`phone` bigint(20) unsigned NOT NULL," .
+        "`uid` mediumint(8) unsigned NOT NULL," .
+        "`country_code` char(40) NOT NULL default '86',".
+        "PRIMARY KEY (`phone`)," .
+        "UNIQUE KEY (`uid`)".
+        ") ENGINE=InnoDB DEFAULT CHARSET=utf8";
+
+        runquery($sql);
+    }
+
+    public static function migrationData($data) {
+        if(isset($data['name']) && strlen($data['name']) != 0) {
+            try {
+                $members = DB::fetch_all("SELECT * FROM ".DB::table($data['name'])); // if table not exists will throw exception
+                for($i = 0; $i < count($members); $i++) {
+                    $member = $members[$i];
+                    if(!isset($member[$data['phone_key']])) {
+                        return '&#23383;&#27573;'.$data['phone_key'].'&#19981;&#23384;&#22312;';                            
+                    }
+                    if(isset($data['name_key']) && strlen($data['name_key']) != 0) {
+                        $key = $data['name_key'];
+                        if(!isset($member[$key])) {
+                            return '&#23383;&#27573;'.$key.'&#19981;&#23384;&#22312;';                            
+                        }
+                        $uid = C::t('common_member')->fetch_uid_by_username($member[$key]);
+                    } else if (isset($data['id_key']) && strlen($data['id_key']) != 0) {
+                        $key = $data['id_key'];
+                        if(!isset($member[$key])) {
+                            return '&#23383;&#27573;'.$key.'&#19981;&#23384;&#22312;';                            
+                        }
+                        $uid = $member[$key];
+                    } else {
+                        return '&#35831;&#22635;&#20889;&#21807;&#19968;&#23545;&#24212;&#23383;&#27573;&#21517;';
+                    }
+                    C::t('#phone_auth#common_vphone')->save($uid, $member[$data['phone_key']]);
+                }
+                return 'success';
+            } catch (Exception $e) {
+                return $e->getMessage();
+            }
+        } else {
+            return 'success';      
+        }
+    }
+}
+
+Install::createTables();
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $data = $_POST['table'];
+    $res = Install::migrationData($data);
+    if($res == 'success') {
+        $finish = TRUE;
+    } else {
+        $error_msg = $res;
+        include template('phone_auth:install');    
+    }
+} else {
+    include template('phone_auth:install');    
+}
+
