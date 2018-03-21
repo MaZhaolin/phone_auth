42fcb67017e5880d71266b6fdfa7e3938b2bd464
insertSweat <769356353@qq.com>
2018-03-21 16:56:36 +0800

    refactoring set by vue

--- lib/config.php
+++ lib/config.php
@@ -11,7 +11,7 @@ define("PIC_POST_FIX", ".png");
 define("PUBLIC_KEY_PATH", "http://down.vaptcha.com/publickey");
 define("IS_DOWN_PATH", "http://down.vaptcha.com/isdown");
 define("DOWN_TIME_PATH", "downtime/");
-define("VERSION", '1.8.2');
+define("VERSION", '1.8.3');
 define("SDK_LANG", 'php');
 define("API_URL", 'http://api.vaptcha.com');
 define("GET_CHALLENGE_URL", '/challenge');
--- static/js/ug.sh
+++ static/js/ug.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
-uglifyjs set.js -c -m -o set.min.js
-python3 unicode.py set.min.js
-echo 'set.js => set.min.js'
 
-uglifyjs mobile.js -c -m -o mobile.min.js
-python3 unicode.py mobile.min.js
-echo 'mobile.js => mobile.min.js'
-
-uglifyjs v-helper.js -c -m -o v-helper.min.js
-python3 unicode.py v-helper.min.js
-echo 'v-helper.js => v-helper.min.js'
-
-uglifyjs app.js -c -m -o app.min.js
-python3 unicode.py app.min.js
-echo 'app.js => app.min.js'
-
-uglifyjs member.js -c -m -o member.min.js
-python3 unicode.py member.min.js
-echo 'member.js => member.min.js'
+uglifyjs "$1.js" -c -m -o "$1.min.js"
+python3 unicode.py "$1.min.js"
+echo "$1.js => $1.min.js"
 
