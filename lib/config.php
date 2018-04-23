58a15a0d52bba3a4ba04f99e52efdef0e60955d2
insertSweat <769356353@qq.com>
2018-04-23 17:15:34 +0800

    update v1.9.3

--- lib/config.php
+++ lib/config.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 if(!defined('IN_DISCUZ')) {
     exit('Access Denied');
 }
@@ -11,16 +11,10 @@ define("PIC_POST_FIX", ".png");
 define("PUBLIC_KEY_PATH", "http://down.vaptcha.com/publickey");
 define("IS_DOWN_PATH", "http://down.vaptcha.com/isdown");
 define("DOWN_TIME_PATH", "downtime/");
-define("VERSION", '1.9.1');
+define("VERSION", '1.9.3');
 define("SDK_LANG", 'php');
 define("API_URL", 'http://api.vaptcha.com');
 define("GET_CHALLENGE_URL", '/challenge');
 define("VALIDATE_URL", '/validate');
 define("REQUEST_USED_UP", '0209');
 define("DOWNTIME_CHECK_TIME", 185000);
-
-
-
-
-
-
